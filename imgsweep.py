c5e75d7d705b8009204c7a94c6adb6f05d8c4368
Lawrence Adams <lawrencetadams@googlemail.com>
2016-01-04 18:39:53 +0000

    Added directory checking and downloading methods (using urllib)

--- imgsweep.py
+++ imgsweep.py
@@ -1,9 +1,12 @@
 import praw
+import urllib
+import os
 
 # Define Globals
 USER_AGENT = "python: mayr.redditCrawlerApp:v0.0.1"
 DEFAULT_SUBREDDIT = "all"
 DEFAULT_POST_LIMIT = 5 # (Maximum is 100 per PRAW reddit API call.)
+DOWNLOADED_IMAGE_FOLDER = "download"
 
 # Initialise new PRAW instance
 r = praw.Reddit(user_agent=USER_AGENT)
@@ -35,10 +38,31 @@ def filter_links(input_list):
 			pass
 		else:
 			result.append(link)
-
 	return result
+
+"""
+A function that takes a single url as a string, and downloads the associated image.
+"""
+def download_image(image_url):
+	filename = image_url.split('/')
+	urllib.request.urlretrieve(image_url, filename + ".jpg")
+
+"""
+TODO
+
+Find if file is present. Go to lowest int count for name of file.
+ >>> e.g. 00001.jpg is present, so name next file 00002.jpg
+"""
+# def _file_present(directory):
+
+def _is_download_directory_present():
+	if os.path.exists(DOWNLOADED_IMAGE_FOLDER) and os.path.isdir(DOWNLOADED_IMAGE_FOLDER):
+		pass
+	else:
+		os.makedir(DOWNLOADED_IMAGE_FOLDER)
+
 
 
 if __name__ == "__main__":
 	i = get_image_urls(DEFAULT_SUBREDDIT, DEFAULT_POST_LIMIT)
-	# print(i)
+	# print(i)	
